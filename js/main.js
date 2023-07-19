bf8a435a1271ef79c8d01f5c1029c6bfc97be788
Alex <oleksiymiasnikov@gmail.com>
2023-07-19 12:44:12 +0300

    filtering by tags done

--- js/main.js
+++ js/main.js
@@ -1,29 +1,33 @@
 const cupons = JSON.parse(localStorage.getItem("cupons"));
 const tags = JSON.parse(localStorage.getItem("tags"));
 tagsShow(currentTag);
+cuponsShow(cupons);
+fillContantToSelect();
 tagsLength = tags.length;
 
 // output cupons list
-document.querySelector(".cupons-bar").innerHTML = `<ul class="cupons"></ul>`;
-for (let i = 0; i < cupons.length; i++) {
-  let row = document.createElement("li");
-  row.classList.add("cupons-item");
-  row.innerHTML = `
-    <img name="li-1" src="images/cupon.png" alt="" />
-    <span class="cupons-title">
-      ${cupons[i].name}
-      <i class="fa fa-heart" style="font-size: 15px"></i>
-    </span>
-    <span class="cupons-title description">
-      <p>${cupons[i].description}</p>
-      <p>Expires in 3 deys</p>
-    </span>
-    <hr />
-    <span class="cupons-title">
-      <p>${cupons[i].price}</p>
-      <button>Add to Cart</button>
-    </span>`;
-  document.querySelector(".cupons").appendChild(row);
+function cuponsShow(cuponsToShow) {
+  document.querySelector(".cupons-bar").innerHTML = `<ul class="cupons"></ul>`;
+  for (let i = 0; i < cuponsToShow.length; i++) {
+    let row = document.createElement("li");
+    row.classList.add("cupons-item");
+    row.innerHTML = `
+      <img name="li-1" src="images/cupon.png" alt="" />
+      <span class="cupons-title">
+        ${cuponsToShow[i].name}
+        <i class="fa fa-heart" style="font-size: 15px"></i>
+      </span>
+      <span class="cupons-title description">
+        <p>${cuponsToShow[i].description}</p>
+        <p>Expires in 3 deys</p>
+      </span>
+      <hr />
+      <span class="cupons-title">
+        <p>${cuponsToShow[i].price}</p>
+        <button>Add to Cart</button>
+      </span>`;
+    document.querySelector(".cupons").appendChild(row);
+  }
 }
 
 // output tags list
@@ -35,8 +39,42 @@ function tagsShow(start) {
     let row = document.createElement("li");
     row.classList.add("category-item");
     row.innerHTML = `
-    <img name="li-1" src="images/category.png" alt="" />          
+    <img name="li-1" src="images/category.png" alt="" onclick="changingSelect('${tags[i]}')"/>          
     <span>${tags[i]}</span>`;
     document.querySelector(".tags").appendChild(row);
   }
+}
+
+// filling select by tags
+function fillContantToSelect() {
+  for (let i = 0; i < tags.length; i++) {
+    let row = document.createElement("option");
+    row.value = `${tags[i]}`;
+    row.innerHTML = `${tags[i]}`;
+    document
+      .querySelector(".center-field")
+      .querySelector(".select_categories")
+      .appendChild(row);
+  }
+}
+
+function changingSelect(selectedTag) {
+  const select = document
+    .querySelector(".center-field")
+    .querySelector(".select_categories");
+  select.value = selectedTag;
+  tagIsSelected();
+}
+
+function tagIsSelected() {
+  let cuponsFiltered;
+  let selectedTag = document.getElementById("select_categories").value;
+  if (selectedTag) {
+    cuponsFiltered = cupons.filter((cupon) => {
+      return cupon.tags.includes(selectedTag);
+    });
+  } else {
+    cuponsFiltered = cupons;
+  }
+  cuponsShow(cuponsFiltered);
 }
